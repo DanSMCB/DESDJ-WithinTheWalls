365f34551206cbc92096aa605e5fa587a7010c82
Daniel Braga <daniel.carvalho.braga@gmail.com>
2025-11-26 19:39:06 +0000

    added flashlight

---
+++ WithinTheWalls/Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+    [SerializeField] AudioClip onSFX;
+    [SerializeField] AudioClip offSFX;
+
+    private Camera cam;
+    private GameObject lightSource;
+    private AudioSource audioSource;
+    private Vector3 offset;
+
+    public bool IsOn { get; private set; }
+    private readonly float speed = 5f;
+
+    public bool IsEnabled = true;
+
+
+    private void Awake()
+    {
+        cam = GetComponentInChildren<Camera>();
+        lightSource = transform.GetChild(3).gameObject;
+        audioSource = GetComponentInChildren<AudioSource>();
+
+    }
+
+    private void Start()
+    {
+        lightSource.gameObject.SetActive(false);
+        offset = transform.position - cam.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = cam.transform.position + offset;
+        transform.rotation = Quaternion.Slerp(transform.rotation, cam.transform.rotation, speed * Time.deltaTime);
+
+        if (!IsEnabled)
+        {
+            lightSource.gameObject.SetActive(false);
+            IsOn = false;
+            return;
+        }
+
+        if (Input.GetButtonDown("Flashlight"))
+        {
+
+            if (IsOn == false)
+            {
+                lightSource.gameObject.SetActive(true);
+                IsOn = true;
+                audioSource.PlayOneShot(onSFX);
+            }
+            else
+            {
+                lightSource.gameObject.SetActive(false);
+                IsOn = false;
+                audioSource.PlayOneShot(offSFX);
+            }
+        }
+    }
+
+    public void PlayFlashlightOffSFX()
+    {
+        audioSource.PlayOneShot(onSFX);
+        audioSource.PlayDelayed(2f);
+        audioSource.PlayOneShot(offSFX);
+    }
+}
